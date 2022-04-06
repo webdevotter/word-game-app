fcbce35f5dbde7acbfbddbbe9552fb3ecbdb453e
webdevotter <webdevotter@gmail.com>
2022-04-05 21:13:42 -0500

    Initial setup
    created variables and functions per study guide comment on extra credit (css and js)

---
+++ js/app.js
@@ -0,0 +1,39 @@
+// Create needed variables
+let qwerty = document.getElementById('qwerty');
+let phrase = document.getElementById('phrase');
+let buttonReset = getElementsByClassName('btn_reset');
+let missed = 0;
+
+// Create an array named phrases
+
+
+// Attach an event listener to the Start Game button to hide the start screen overlay
+
+
+// Create a getRandomPhrasAsArray function
+
+function getRandomPhraseAsArray () {
+
+
+};
+
+// Create an addPhraseToDisplay function
+
+function addPhraseToDisplay() {
+
+
+};
+
+// Create a checkLetter function
+
+function checkLetter() {
+
+
+};
+
+// Add an event listener to the keyboard
+
+
+// Create a checkWin function
+
+//Extra Credit
