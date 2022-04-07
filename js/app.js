3462ba2a509998b532aadce9238a567cfe331d5f
webdevotter <webdevotter@gmail.com>
2022-04-06 20:42:20 -0500

    startButton Event Listener

--- js/app.js
+++ js/app.js
@@ -1,39 +1,60 @@
 // Create needed variables
+
 let qwerty = document.getElementById('qwerty');
 let phrase = document.getElementById('phrase');
-let buttonReset = getElementsByClassName('btn_reset');
+let startButton = document.getElementsByClassName('btn_reset');
 let missed = 0;
 
 // Create an array named phrases
+// Source: https://www.ef.edu/english-resources/english-idioms/
 
+let phrases = [
+"blessing in disguise",
+"dime a dozen",
+"better late than never",
+"bite the bullet",
+"easy does it",
+"benefit of the doubt",
+"back to the drawing board",
+"the last straw",
+"under the weather",
+"you can say that again",
+];
 
-// Attach an event listener to the Start Game button to hide the start screen overlay
+// return a random phrase from an array
+const getRandomPhrasAsArray = arr => {
+// generates random number between 0 and length of the array    
+let randomNumber = (Math.floor(Math.random() * phrases.length));
+let randomPhrase = phrases[randomNumber];
+return(randomPhrase);
+}
 
+// adds the letters of a string to the display 
+const addPhraseToDisplay = arr => {
 
-// Create a getRandomPhrasAsArray function
+}
 
-function getRandomPhraseAsArray () {
+// check if a letter is in the phrase 
+const checkLetter = button => {
 
+}
 
-};
+// check if the game has been won or lost
+const checkWin = () => {
 
-// Create an addPhraseToDisplay function
+}
 
-function addPhraseToDisplay() {
+// listen for the start game button to be pressed
+// add event listener to button with id btn_reset and global variable startButton
+addEventListener('click',(startButton) => {
+    // define overlay variable 
+    let overlay = document.getElementById('overlay');
+    //set style of overlay to none
+    overlay.style.display = 'none'; 
+});
 
+//listen for the onscreen keyboard to be clicked
+qwerty.addEventListener('click', e => {
 
-};
+});
 
-// Create a checkLetter function
-
-function checkLetter() {
-
-
-};
-
-// Add an event listener to the keyboard
-
-
-// Create a checkWin function
-
-//Extra Credit
