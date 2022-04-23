700b5d5d2151994a5c9233948427b95aea0c3124
webdevotter <webdevotter@gmail.com>
2022-04-23 16:49:38 -0500

    updated variables, functions

--- js/app.js
+++ js/app.js
@@ -1,17 +1,17 @@
 // Create needed variables
 
-let qwerty = document.getElementById('qwerty');
-let phrase = document.getElementById('phrase');
-let startButton = document.getElementsByClassName('btn_reset');
+const qwerty = document.getElementById('qwerty');
+const phrase = document.getElementById('phrase');
+const startButton = document.getElementsByClassName('btn__reset');
+const overlay = document.getElementById('overlay');
 let missed = 0;
+
 
 
 // Create an array named phrases
 // Source: https://www.ef.edu/english-resources/english-idioms/
 
-let phrases = [
-// index 0 never chosen due to random number generator being set to 1
-"",
+const phrases = [
 "blessing in disguise",
 "dime a dozen",
 "better late than never",
@@ -24,43 +24,95 @@ let phrases = [
 "you can say that again",
 ];
 
+// listen for the start game button to be pressed
+// add event listener to button with id btn_reset and global variable startButton
+addEventListener('click', (startButton) => {
+      //set style of overlay to none
+    overlay.style.display = 'none'; 
+});
 
 // return a random phrase from an array
-let getRandomPhraseAsArray = (phrases) => {
+function getRandomPhraseAsArray(phrases) {
 // generates random number between 1 and length of the array    
 let randomNumber = Math.floor((Math.random() * phrases.length) + 1);
 // assigns index value of array to variable randomPhrase
 let randomPhrase = phrases[randomNumber];
 // returns randomPhrase
-return(randomPhrase);
+return randomPhrase;
 }
 
 // adds the letters of a string to the display 
-const addPhraseToDisplay = arr => {
-
+function addPhraseToDisplay(randomPhrase) {
+    for (let i = 0; i < randomPhrase.length; i++ ) {
+        let li = document.createElement('li');
+        li.textContent = randomPhrase[i];
+        let ul = document.querySelector('#phrase ul');
+        ul.appendChild(li);
+            if (arr[i] !== '') {
+                li.className = 'letter';
+            } else { 
+                li.className = 'space';
+            }
+    }
 }
 
-// check if a letter is in the phrase 
-const checkLetter = button => {
 
+//listen for the onscreen keyboard to be clicked
+qwerty.addEventListener('click', (e) => {
+    let target = e.target;
+    let letterMatch = checkLetter(target);
+    let hearts = document.querySelectorAll('img');
+        if (target.className === 'button') {
+            target.className === 'chosen';
+            target.setAttribute('disabled', '');
+        }
+        if (letterMatch === null) {
+           hearts[missed].setAttribute('src', 'images/lostHeart.png');
+           missed++;
+        }
+    checkWin();
+});
+
+
+// // check if a letter is in the phrase 
+const checkLetter = button => {
+    let listEl = document.getElementsByClassName('letter');
+    let match = null;
+        for (let i = 0; i < listEl.length; i++) {
+            if (listEl[i].textContent.toLowerCase() === button.textContent.toLowerCase()) {
+                listEl[i].classList.add('show');
+                listEl[i].classList.add('transition');
+                match = button.textContent;
+            }
+        }
+    return match;
 }
 
 // check if the game has been won or lost
-const checkWin = () => {
+function checkWin() {
+    let listEl = document.getElementsByClassName('letter');
+    let show = document.getElementsByClassName('show');
+    let ul = document.getElementById('unList');
+    
+    if (listEl. length === show.length && missed < 4) {
+        overlay.style.display = 'flex';
+        overlay.className = 'win';
+        title.textContent = 'You Won';
+        ul.style.display = 'none';
+        startButton.textContent = 'Start Over';
+    } else {
+        overlay.style.display = 'flex';
+        overlay.className = 'lose';
+        title.textContent = 'You Lost';
+        ul.style.display = 'none';
+        startButton.textContent = 'Start Over';
+        startOver();
+    }
 
 }
 
-// listen for the start game button to be pressed
-// add event listener to button with id btn_reset and global variable startButton
-addEventListener('click',(startButton) => {
-    // define overlay variable 
-    let overlay = document.getElementById('overlay');
-    //set style of overlay to none
-    overlay.style.display = 'none'; 
-});
-
-//listen for the onscreen keyboard to be clicked
-qwerty.addEventListener('click', e => {
-
-});
-
+function startOver() {
+        startButton.addEventListener('click', () => {
+        start.reload();
+    });
+}
